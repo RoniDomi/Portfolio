81bce142ee9a69e275109e39b96b0fdfbe95ae3f
ronidomi <roni.domi001@umb.edu>
2025-10-31 14:20:31 -0400

    Css updates, home page almost finished

--- src/screen.tsx
+++ src/screen.tsx
@@ -1,13 +1,27 @@
+import Planet from './planet';
 import './screen.css'
+import TopNav from './topnav';
 
 function Screen() {
   return (
     <>
         <div id="screen">
             <img src="./src/assets/scanlines.png" id="scan" className="noselect" />
-            <img src="./src/assets/bezel.png" id="bezel" className="noselect" />
+            <img src="./src/assets/grid.jpg" id="grid" className="noselect" />
             <div id="content">
-                <h1 id="title">&lt;ronidomi&gt;</h1>
+                <TopNav/>
+                <div id="welcome">
+                    <h1>Hello World, I'm &lt;Roni/&gt;</h1>
+                    <p>I'm a computer sience student, and aspiring software engineer!<br/>
+                       I love building creative and technical projects, ranging from <br/>
+                       low level systems, to visual tools, and full stack projects.  <br/> 
+                       Creating is a passion for me, and coding helps me express this!
+                    </p>
+                </div>
+
+                <div id="planet">
+                    <Planet/>
+                </div>
             </div>
         </div>
     </>
---
+++ src/topnav.tsx
@@ -0,0 +1,18 @@
+import './screen.css'
+
+function TopNav() {
+    return(
+        <>
+            <div id="topnav">
+                <ul>
+                    <li>About Me</li>
+                    <li>Projects</li>
+                    <li>Resume</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default TopNav;
