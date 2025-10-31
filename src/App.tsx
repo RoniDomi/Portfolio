f0298c91464913c000aafaaf76da2e9f4ac15448
ronidomi <roni.domi001@umb.edu>
2025-10-31 02:12:24 -0400

    Scan line effect added, title added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import Screen from './screen.tsx'
 
 function App() {
   return (
     <>
+      <Screen/>
     </>
   )
 }
--- src/screen.tsx
+++ src/screen.tsx
@@ -1,10 +1,14 @@
-import 'screen.css'
+import './screen.css'
 
 function Screen() {
   return (
     <>
         <div id="screen">
-            <img src="assets/scanlines.png" id="scan" />
+            <img src="./src/assets/scanlines.png" id="scan" className="noselect" />
+            <img src="./src/assets/bezel.png" id="bezel" className="noselect" />
+            <div id="content">
+                <h1 id="title">&lt;ronidomi&gt;</h1>
+            </div>
         </div>
     </>
   )
